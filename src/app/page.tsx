946a1b97677f63030d06e86352894c3d1b542fbb
RohitSingh-dev <rohitsingh20082002@gmail.com>
2025-11-03 00:24:33 +0530

    fixed projects details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,19 +132,19 @@ export default function Page() {
           <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
             {DATA.projects.map((project, id) => (
               <BlurFade
-                key={project.title}
+                key={project?.title}
                 delay={BLUR_FADE_DELAY * 12 + id * 0.05}
               >
                 <ProjectCard
-                  href={project.href}
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  dates={project.dates}
-                  tags={project.technologies}
-                  image={project.image}
-                  video={project.video}
-                  links={project.links}
+                  href={project?.href}
+                  key={project?.title}
+                  title={project?.title ?? "Untitled Project"}
+                  description={project?.description ?? "No description provided."}
+                  dates={project?.dates ?? ""}
+                  tags={project?.technologies ?? []}
+                  image={project?.image}
+                  video={project?.video}
+                  links={project?.links}
                 />
               </BlurFade>
             ))}
